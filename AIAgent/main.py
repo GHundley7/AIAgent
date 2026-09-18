8885ba215bf7e55ad70ee78c9df2e9692a14da2c
GHundley7 <granthundley7@gmail.com>
2026-09-18 18:47:46 -0400

    A: Read API key to reach Openrouter

---
+++ AIAgent/main.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("OPENROUTER_API_KEY")
+if api_key == None:
+    raise RuntimeError("API Key was not found")
+
+from openai import OpenAI
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
+
+response = client.chat.completions.create(
+    model="openrouter/free",
+    messages=[
+        {
+            "role": "user",
+            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        }
+    ],
+)
+
+print(response.choices[0].message.content)
